c542c1fc9a027bb71f64bfece35d3cccbda644e9
Maksym Shkarbun <suprime28@yahoo.com>
2023-11-01 18:30:36 +0200

    New arrays

--- src/main/java/com/javarush/CaesarCipher.java
+++ src/main/java/com/javarush/CaesarCipher.java
@@ -5,6 +5,7 @@ public class CaesarCipher {
     public void BruteForce(){}
     public void Encrypt(){
         char[] symols = new char[]{};
+        char[] upperSymbols = new char[]{};
     }
     public void Decrypt(){}
 }
